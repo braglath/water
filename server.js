0c194517cb7d62e2b70304a57a0975318be437ab
bragu <87694668+braglath@users.noreply.github.com>
2022-06-21 00:47:38 +0530

    get details with jwt token and id

--- server.js
+++ server.js
@@ -12,12 +12,18 @@ const registerRoute = require("./routes/register_route");
 const errorHandler = require("./middlewares/error_handler");
 const userCRUDRoute = require("./routes/crud_user_route");
 const loginRoute = require("./routes/login_route");
+const loginWPwdRoute = require("./routes/login_w_password_route");
+const verifyJWT = require("./middlewares/jwt_verify_middleware");
 
 require("./config/mysql_config"); //? this will automatically connect to the database
 
-app.use("/api/user", userCRUDRoute);
 app.use("/api/user/register", registerRoute);
 app.use("/api/user/login", loginRoute);
+app.use("/api/user/login-password", loginWPwdRoute);
+
+//? need token for below routes
+app.use(verifyJWT);
+app.use("/api/user", userCRUDRoute);
 app.use(errorHandler);
 
 app.listen(port, () =>
